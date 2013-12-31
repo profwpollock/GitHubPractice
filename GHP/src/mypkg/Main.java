f993d3fd565d61d209437640445f31d7e3101100
Wayne Pollock <pollock@acm.org>
2013-12-30 23:55:11 -0500

    Updated Main from NetBeans

--- GHP/src/mypkg/Main.java
+++ GHP/src/mypkg/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println( "Hello from GHP Eclipse Project!" );
+                System.out.println("odified from NetBeans");
 	}
 
 }
